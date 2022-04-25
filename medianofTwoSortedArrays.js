ed530d7d2c78d73b660555788709ce18e61ab0db
Nestor <ndqa96@gmail.com>
2022-04-24 23:01:14 -0400

    Median two sorted Arrays VFINAL

---
+++ medianofTwoSortedArrays.js
@@ -0,0 +1,26 @@
+/*Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+Example 1:
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+
+Example 2:
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.*/
+let testArr = [];
+
+function firstStep(nums1, nums2){
+
+    nums1.forEach(function(item, index){
+    testArr.push(item);
+    })
+    nums2.forEach(function(item, index){
+        testArr.push(item);
+    })
+    let finalResult = toInteger((testArr + 1)/2);
+
+    return parseInt(finalResult);
+}
